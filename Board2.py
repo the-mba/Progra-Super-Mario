a7370ad7ba7d6266e1b36bb2f2f155d29bbd594d
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-12-10 12:20:53 +0100

    --- Auto-Git Commit ---

--- Board2.py
+++ Board2.py
@@ -2,6 +2,10 @@ from random import randint
 
 import pyxel
 
+PLAYER_STARTING_X = 40
+PLAYER_STARTING_Y = 82
+PLAYER_STARTING_VEL_Y = 0
+GRAVITY = 0.38
 
 class App:
     def __init__(self):
@@ -10,15 +14,10 @@ class App:
         pyxel.load("assets/marioassets_133.pyxres")
 
         self.score = 0
-        self.player_x = 72
-        self.player_y = -16
-        self.player_vy = 0
-        self.player_is_alive = True
-
-        self.far_cloud = [(-10, 75), (40, 65), (90, 60)]
-        self.near_cloud = [(10, 25), (70, 35), (120, 15)]
-        self.floor = [(i * 60, randint(8, 104), True) for i in range(4)]
-        self.fruit = [(i * 60, randint(0, 104), randint(0, 2), True) for i in range(4)]
+        self.player_x = PLAYER_STARTING_X
+        self.player_y = PLAYER_STARTING_Y
+        self.player_vel_y = PLAYER_STARTING_VEL_Y
+        self.player_alive = True
 
         pyxel.playm(0, loop=True)
 
@@ -27,28 +26,25 @@ class App:
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
-
+        
         self.update_player()
 
-        for i, v in enumerate(self.floor):
-            self.floor[i] = self.update_floor(*v)
-
-        for i, v in enumerate(self.fruit):
-            self.fruit[i] = self.update_fruit(*v)
-
     def update_player(self):
-        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD_1_LEFT):
-            self.player_x = max(self.player_x - 2, 0)
-
-        if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD_1_RIGHT):
+        if pyxel.btnp(pyxel.KEY_RIGHT, 1, 1):
             self.player_x = min(self.player_x + 2, pyxel.width - 16)
+        if pyxel.btnp(pyxel.KEY_LEFT, 1, 1):
+            self.player_x = max(self.player_x - 2, 0)
+        if pyxel.btnp(pyxel.KEY_UP, 1, 1):
+            self.player_vel_y += -2
+        
+        self.player_y = max(self.player_y + self.player_vel_y, HEIGHT)
+        self.player_vel_y += GRAVITY
+        
 
-        self.player_y += self.player_vy
-        self.player_vy = min(self.player_vy + 1, 8)
 
         if self.player_y > pyxel.height:
-            if self.player_is_alive:
-                self.player_is_alive = False
+            if self.player_alive:
+                self.player_alive = False
                 pyxel.play(3, 5)
 
             if self.player_y > 600:
@@ -56,49 +52,7 @@ class App:
                 self.player_x = 72
                 self.player_y = -16
                 self.player_vy = 0
-                self.player_is_alive = True
-
-    def update_floor(self, x, y, is_active):
-        if is_active:
-            if (
-                self.player_x + 16 >= x
-                and self.player_x <= x + 40
-                and self.player_y + 16 >= y
-                and self.player_y <= y + 8
-                and self.player_vy > 0
-            ):
-                is_active = False
-                self.score += 10
-                self.player_vy = -12
-                pyxel.play(3, 3)
-        else:
-            y += 6
-
-        x -= 4
-
-        if x < -40:
-            x += 240
-            y = randint(8, 104)
-            is_active = True
-
-        return x, y, is_active
-
-    def update_fruit(self, x, y, kind, is_active):
-        if is_active and abs(x - self.player_x) < 12 and abs(y - self.player_y) < 12:
-            is_active = False
-            self.score += (kind + 1) * 100
-            self.player_vy = min(self.player_vy, -8)
-            pyxel.play(3, 4)
-
-        x -= 2
-
-        if x < -40:
-            x += 240
-            y = randint(0, 104)
-            kind = randint(0, 2)
-            is_active = True
-
-        return (x, y, kind, is_active)
+                self.player_alive = True
 
     def draw(self):
         pyxel.cls(12)
@@ -106,18 +60,16 @@ class App:
         # draw sky
         pyxel.bltm(0, 50, 0, 0, 88, 160, 32)
 
-        
-
         # draw player
         pyxel.blt(
             self.player_x,
             self.player_y,
             0,
-            16 if self.player_vy > 0 else 0,
-            0,
+            0 if self.player_vel_y > 0 else 0,
+            48,
             16,
             16,
-            12,
+            12
         )
 
         # draw score
